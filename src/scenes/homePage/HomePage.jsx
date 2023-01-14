eaea3347444dab14ec479e8ca167e09384044d3c
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-14 15:55:57 -0600

    Add navbar to homepage for now: make PageContainer later

--- src/scenes/homePage/HomePage.jsx
+++ src/scenes/homePage/HomePage.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import NavBar from 'scenes/navBar/NavBar'
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+      <NavBar />
+      <h2>Homepage</h2>
+    </div>
   )
 }
