c8bd0218a0580719fe8c0f6b2f8e6d919f02a92b
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-14 15:54:31 -0600

    Write basic header

--- src/scenes/navBar/NavBar.jsx
+++ src/scenes/navBar/NavBar.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setLogin, setLogout, setMode } from 'state';
+import { MdMessage } from 'react-icons/md'
+import {BsFillMoonFill, BsFillQuestionCircleFill, BsFillBellFill  } from 'react-icons/bs'
 
 export default function NavBar() {
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const user = useSelector((state) => state.user)
+  const [dropDown, setDropDown] = React.useState(false)
+
   return (
-    <div>NavBar</div>
+    <div className='navbar'>
+      <div className='left-nav'>
+        <h1>OpenBar</h1>
+        <input className='search'></input>
+      </div>
+      <div className='right-nav'>
+        <BsFillMoonFill size={"3vh"} onClick={()=> dispatch(setMode())}/>
+        <MdMessage size={"3vh"} />
+        <BsFillBellFill size={"3vh"}/>
+        <BsFillQuestionCircleFill size={"3vh"} onClick={() => dispatch(setLogout())}/>
+        <div className='dropdown'>
+          <button onClick={() => (setDropDown(!dropDown))}> {user ? <p>{`${user.firstName} ${user.lastName}`}</p> : <p onClick={() => dispatch(setLogin())}>Login</p>}</button>
+          {dropDown && <p>Active Dropdown</p>}
+        </div>
+      </div>
+    </div>
   )
 }
