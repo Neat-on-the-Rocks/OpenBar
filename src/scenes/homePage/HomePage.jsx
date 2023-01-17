4721bf06895a984b0c0895bc2cfadb7c6104c95f
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-16 18:36:23 -0600

    Pass down picture path

--- src/scenes/homePage/HomePage.jsx
+++ src/scenes/homePage/HomePage.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import NavBar from 'scenes/navBar/NavBar'
+import PostWidget from 'scenes/widgets/PostWidget'
+import UserPostWidget from 'scenes/widgets/UserPostWidget'
 
 export default function HomePage() {
+  const { _id, picturePath } = useSelector((state) => state.user)
   return (
     <div>
       <NavBar />
       <h2>Homepage</h2>
+      <UserPostWidget picturePath={picturePath} />
+      <PostWidget userId={_id} />
     </div>
   )
 }
