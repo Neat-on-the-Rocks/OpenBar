6fec09cc6b3350e5732f636f590e3c5409ffb152
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-16 21:07:58 -0600

    Refactor widget

--- src/scenes/widgets/PostsWidget.jsx
+++ src/scenes/widgets/PostsWidget.jsx
@@ -38,31 +38,8 @@ export default function PostsWidget({userId, isProfile = false}) {
       }, [])
   return (
     <div>
-        {posts.map(
-        ({
-          _id,
-          userId,
-          firstName,
-          lastName,
-          description,
-          location,
-          picturePath,
-          userPicturePath,
-          likes,
-          comments,
-        }) => (
-          <PostWidget
-            key={_id}
-            postId={_id}
-            postUserId={userId}
-            name={`${firstName} ${lastName}`}
-            description={description}
-            location={location}
-            picturePath={picturePath}
-            userPicturePath={userPicturePath}
-            likes={likes}
-            comments={comments}
-          />
+        {posts.map(({_id,userId,firstName,lastName,description,location,picturePath,userPicturePath,likes,comments,}) => (
+          <PostWidget key={_id} postId={_id} postUserId={userId} name={`${firstName} ${lastName}`} description={description} location={location} picturePath={picturePath} userPicturePath={userPicturePath} likes={likes} comments={comments} />
         )
       )}
     </div>
