4b5af3ca1c82cf590c338025ee7ac55d3ce26898
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-17 20:48:35 -0600

    Render Profile widget

--- src/scenes/homePage/HomePage.jsx
+++ src/scenes/homePage/HomePage.jsx
@@ -3,15 +3,18 @@ import { useSelector } from 'react-redux'
 import NavBar from 'scenes/navBar/NavBar'
 import PostsWidget from 'scenes/widgets/PostsWidget'
 import UserPostWidget from 'scenes/widgets/UserPostWidget'
+import ProfileWidget from 'scenes/widgets/ProfileWidget'
+
 
 export default function HomePage() {
-  const { _id, picturePath } = useSelector((state) => state.user)
+  const { _id, picturePath, location, occupation, firstName, lastName} = useSelector((state) => state.user)
   return (
     <div>
       <NavBar />
       <h2>Homepage</h2>
       <UserPostWidget picturePath={picturePath} />
       <PostsWidget userId={_id} />
+      <ProfileWidget picturePath={picturePath} location={location} occupation={occupation} name={firstName + ' ' +lastName}/>
     </div>
   )
 }
