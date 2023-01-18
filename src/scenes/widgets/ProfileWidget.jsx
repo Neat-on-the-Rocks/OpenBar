63b4c9ea57406d9be4a26b572cba06a774c4d43e
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-18 16:58:23 -0600

    Style Profile Widget

--- src/scenes/widgets/ProfileWidget.jsx
+++ src/scenes/widgets/ProfileWidget.jsx
@@ -6,10 +6,12 @@ import {BsBriefcaseFill} from 'react-icons/bs'
 export default function ProfileWidget({picturePath, location, occupation,  name}) {
   return (
     <div className='profile-widget'>
-        <div className='name'>
-            <img src={`http://localhost:5000/assets/${picturePath}`} alt=""/>
-            <h3>{name}</h3>
-            <RiUserSettingsFill />
+        <div className='user-info'>
+            <div className='user'>
+                <img src={`http://localhost:5000/assets/${picturePath}`} alt=""/>
+                <h3>{name}</h3>
+            </div>
+            <RiUserSettingsFill size={25} />
         </div>
         <div className="icon-text">
             <MdLocationPin />
@@ -19,6 +21,10 @@ export default function ProfileWidget({picturePath, location, occupation,  name}
             <BsBriefcaseFill />
             <h4>{occupation}</h4>
         </div>
+        <hr />
+        <h4 className="space-between"><p>Profile Views</p> <p>100</p></h4>
+        <h4 className="space-between"><p>Friends</p> <p>0</p></h4>
+        <hr />
     </div>
   )
 }
