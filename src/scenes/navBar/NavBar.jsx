a0755c734260caf046f491ce3ead619bb637f770
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-18 19:02:21 -0600

    Style navbar

--- src/scenes/navBar/NavBar.jsx
+++ src/scenes/navBar/NavBar.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch} from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { setLogin, setLogout, setMode } from 'state';
-import { MdMessage } from 'react-icons/md'
-import {BsFillMoonFill, BsFillQuestionCircleFill, BsFillBellFill  } from 'react-icons/bs'
+import { setLogout, setMode } from 'state';
+import { MdMessage, MdLogout} from 'react-icons/md'
+import {BsFillMoonFill, BsFillQuestionCircleFill  } from 'react-icons/bs'
 
 export default function NavBar() {
 
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  const user = useSelector((state) => state.user)
-  const [dropDown, setDropDown] = React.useState(false)
+
+  const toggleLogout = () => {
+    dispatch(setLogout())
+    navigate("/")
+  }
+
 
   return (
     <div className='navbar'>
       <div className='left-nav'>
         <h1>OpenBar</h1>
-        <input className='search'></input>
+        <input className='search' placeholder="Search" />
       </div>
       <div className='right-nav'>
         <BsFillMoonFill size={"3vh"} onClick={()=> dispatch(setMode())}/>
         <MdMessage size={"3vh"} />
-        <BsFillBellFill size={"3vh"}/>
-        <BsFillQuestionCircleFill size={"3vh"} onClick={() => dispatch(setLogout())}/>
-        <div className='dropdown'>
-          <button onClick={() => (setDropDown(!dropDown))}> {user ? <p>{`${user.firstName} ${user.lastName}`}</p> : <p onClick={() => dispatch(setLogin())}>Login</p>}</button>
-          {dropDown && <p>Active Dropdown</p>}
-        </div>
+        <BsFillQuestionCircleFill size={"3vh"} onClick={() => { window.location.href = "https://github.com/aldoportillo/OpenBar"}} />
+        <MdLogout size={"3vh"} onClick={toggleLogout}/>
       </div>
     </div>
   )
