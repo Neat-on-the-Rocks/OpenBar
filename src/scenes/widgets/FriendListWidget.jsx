53ef2740e1cca580677d0d53444d862334c748b0
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-18 20:43:30 -0600

    Create and render friend list

---
+++ src/scenes/widgets/FriendListWidget.jsx
@@ -0,0 +1,40 @@
+import FriendBox from 'components/FriendBox';
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setFriends } from 'state';
+import {BsFillPersonPlusFill, BsFillPersonDashFill } from 'react-icons/bs'
+
+export default function FriendListWidget({userId}) {
+
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.token);
+  const friends = useSelector((state) => state.user.friends);
+
+  const getFriends = async () => {
+    const response = await fetch(
+      `http://localhost:5000/users/${userId}/friends`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const data = await response.json();
+    dispatch(setFriends({ friends: data }));
+  };
+
+  React.useEffect(() => {
+    getFriends();
+  }, []);
+
+    console.log(friends)
+  return (
+    <div>
+      <h2>Friends List</h2>
+    {friends.map((friend) => {
+      return <FriendBox key={`key${friend._id}`} friendId={friend._id} friendName={`${friend.firstName} ${friend.lastName}`} occupation={friend.occupation} picturePath={friend.picturePath}/>
+    })}
+    </div>
+  )
+}
+
+
