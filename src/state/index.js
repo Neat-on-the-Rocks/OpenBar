438578948f6e26726fcf83f5681b70a6ba4e908a
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-20 01:13:14 -0600

    Fix bug

--- src/state/index.js
+++ src/state/index.js
@@ -34,13 +34,11 @@ export const authSlice = createSlice({
         },
         setPost: (state, action) => {
             const updatedPosts = state.posts.map((post) => {
-                if(post._id === action.payload.post_id) {
-                    return action.payload.post
-                }
-                return post //Using this exit might cause a bug but we will see
-            })
-            state.posts = updatedPosts
-        }
+              if (post._id === action.payload.post._id) return action.payload.post;
+              return post;
+            });
+            state.posts = updatedPosts;
+          },
     }
 })
 
