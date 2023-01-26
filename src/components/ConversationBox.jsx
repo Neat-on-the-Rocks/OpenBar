9121206b80829a547193fbf55bb468a426a63c81
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-26 16:08:52 -0600

    Call getMessages passed from props on form submit

--- src/components/ConversationBox.jsx
+++ src/components/ConversationBox.jsx
@@ -5,6 +5,9 @@ import { useNavigate } from 'react-router-dom'
 export default function ConversationBox({id, members}) {
     const navigate = useNavigate()
     const userId = useSelector((state) => state.user._id)
+    const [userInfo, setUserInfo] = React.useState("")
+    const token = useSelector((state) => state.token);
+
     const renderUser = members.map(member => {
         if(member !== userId){
             return member
@@ -12,8 +15,23 @@ export default function ConversationBox({id, members}) {
         return null
     })
 
-    console.log(id);
+    const getUser = async (id) => {
+      const res = await fetch(
+          `http://localhost:5000/users/${id}`,
+          {
+              method: "GET",
+              headers: { Authorization: `Bearer ${token}` },
+          }
+      );
+          const user = await res.json()
+          setUserInfo(user)
+    }
 
+    React.useEffect(()=> {
+      getUser(id)
+    },[])
+
+    console.log(userInfo);
 
   return (
     <div onClick={() => navigate(`/messenger/${id}`)}>{renderUser}</div>
