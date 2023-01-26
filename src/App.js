1a077c7270753b6bbd67a52905b177ff9eca5239
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-01-25 18:54:00 -0600

    Create routes for conversations and messages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import Messenger from "components/Messenger";
 import React from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ConversationsPage from "scenes/conversationsPage/ConversationsPage";
 import HomePage from 'scenes/homePage/HomePage'
 import LoginPage from "scenes/loginPage/LoginPage";
 import ProfilePage from "scenes/profilePage/ProfilePage";
@@ -16,6 +18,8 @@ function App() {
         <Route exact path="/" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/profile/:userId" element={<ProfilePage />} />
+        <Route path="/conversations/:userId" element={<ConversationsPage />}/>
+        <Route path="/messenger/:conversationId" element={<Messenger />} />
       </Routes>
       </BrowserRouter>
     </div>
